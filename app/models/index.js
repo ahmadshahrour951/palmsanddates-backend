1d3887e5c131b9632db9533a05c6567e174ec3ba
shahrour <ahmadshahrour951@gmail.com>
2021-06-12 14:30:23 -0700

    Add model relationships

--- app/models/index.js
+++ app/models/index.js
@@ -22,6 +22,27 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
+db.users = require('./user.model')(sequelize, Sequelize);
+db.schools = require('./school.model')(sequelize, Sequelize);
+db.residences = require('./residence.model')(sequelize, Sequelize);
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// Relationships
+/////////////////////////////////////////////////////////////////////////////////////////
+db.users.belongsTo(db.schools)
+db.schools.hasMany(db.users)
+
+db.users.belongsTo(db.residences);
+db.residences.hasMany(db.users);
+
+db.schools.belongsToMany(db.residences, {
+  through: 'school_residences',
+});
+
+db.residences.belongsToMany(db.schools, {
+  through: 'school_residences',
+});
+
 /////////////////////////////////////////////////////////////////////////////////////////
 // Test database connection
 /////////////////////////////////////////////////////////////////////////////////////////
