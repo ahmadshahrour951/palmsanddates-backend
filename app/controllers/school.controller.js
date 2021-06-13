574cf2f58b3886f53ef4bcdc3450b1f2b9bb1315
shahrour <ahmadshahrour951@gmail.com>
2021-06-12 19:39:28 -0700

    Get route implemented

--- app/controllers/school.controller.js
+++ app/controllers/school.controller.js
@@ -6,6 +6,7 @@ const schoolController = {
   validate,
   createSchool,
   getSchools,
+  getSchool,
 };
 
 function validate(method) {
@@ -34,6 +35,17 @@ async function getSchools(req, res, next) {
     return res
       .status(200)
       .json({ message: 'Schools successfully fetched.', data: { schools } });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function getSchool(req, res, next) {
+  try {
+    const school = await db.schools.findByPk(req.params.id);
+    return res
+      .status(200)
+      .json({ message: 'School successfully fetched.', data: { school } });
   } catch (error) {
     next(error);
   }
