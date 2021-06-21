079b0ca9daaf6c0809b9f7b0d8760e2b354f7f40
shahrour <ahmadshahrour951@gmail.com>
2021-06-20 19:12:56 -0700

    small bug for getUserType

--- app/routes/userType.route.js
+++ app/routes/userType.route.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const { userTypeController } = require('../controllers');
-const { validate, createUserType, getUserTypes, updateUserType } =
+const { validate, createUserType, getUserType, getUserTypes, updateUserType } =
   userTypeController;
 
 router.post('/', validate('createUserType'), createUserType);
 router.get('/', getUserTypes);
 router.get('/:id', getUserType);
-router.put('/:id', validate('updateUserType'), updateUserType)
+router.put('/:id', validate('updateUserType'), updateUserType);
 
 module.exports = router;
