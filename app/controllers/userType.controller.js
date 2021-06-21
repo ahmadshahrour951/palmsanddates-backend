1ef8ca557146d02c15c20585105291bbeffaf480
shahrour <ahmadshahrour951@gmail.com>
2021-06-20 18:56:38 -0700

    createUserType

--- app/controllers/userType.controller.js
+++ app/controllers/userType.controller.js
@@ -3,7 +3,30 @@ const { body } = require('express-validator');
 const db = require('../models');
 
 const userTypeController = {
+  validate,
+  createUserType,
 };
 
+function validate(method) {
+  switch (method) {
+    case 'createUserType':
+      return [body('name').exists().notEmpty().isString().trim()];
+  }
+}
+
+async function createUserType(req, res, next) {
+  try {
+    const newUserType = req.body;
+    const createdUserType = await db.userTypes.create(newUserType);
+    return res.status(201).json({
+      message: 'User Type created successfully.',
+      data: {
+        id: createdUserType.id,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+}
 
 module.exports = userTypeController;
--- app/routes/userType.route.js
+++ app/routes/userType.route.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const { userTypeController } = require('../controllers');
+const { validate, createUserType } = userTypeController;
 
+router.post('/', validate('createUserType'), createUserType);
 
 module.exports = router;
