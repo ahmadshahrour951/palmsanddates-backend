f8e137e305bc6ef3949e65c638a5628a8f9dd41a
shahrour <ahmadshahrour951@gmail.com>
2021-06-20 22:13:39 -0700

    Route & controller setup

---
+++ app/controllers/event.controller.js
@@ -0,0 +1,7 @@
+const { body } = require('express-validator');
+
+const db = require('../models');
+
+const eventController = {};
+
+module.exports = eventController;
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,10 +2,12 @@ const schoolController = require('./school.controller');
 const residenceController = require('./residence.controller');
 const userController = require('./user.controller');
 const userTypeController = require('./userType.controller');
+const eventController = require('./event.controller')
 
 module.exports = {
   schoolController,
   residenceController,
   userController,
   userTypeController,
+  eventController
 };
