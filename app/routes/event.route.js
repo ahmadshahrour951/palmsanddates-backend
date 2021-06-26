4494fffccfa57beea0ab76aec453dad20184f2b0
shahrour <ahmadshahrour951@gmail.com>
2021-06-26 13:16:45 -0700

    leaveEvent

--- app/routes/event.route.js
+++ app/routes/event.route.js
@@ -2,13 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 const { eventController } = require('../controllers');
-const { validate, createEvent, getEvents, getEvent, updateEvent, joinEvent } =
-  eventController;
+const {
+  validate,
+  createEvent,
+  getEvents,
+  getEvent,
+  updateEvent,
+  joinEvent,
+  leaveEvent,
+} = eventController;
 
 router.post('/', validate('createEvent'), createEvent);
 router.get('/', getEvents);
 router.get('/:id', getEvent);
 router.put('/:id', validate('updateEvent'), updateEvent);
 router.post('/:id/join', validate('joinEvent'), joinEvent);
+router.post('/:id/leave', validate('leaveEvent'), leaveEvent);
 
 module.exports = router;
