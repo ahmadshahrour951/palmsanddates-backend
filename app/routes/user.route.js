c4f92726fd3d6a6b60ef63397b67255f8f872412
shahrour <ahmadshahrour951@gmail.com>
2021-06-25 21:01:11 -0700

    route config

--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -2,11 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 const { userController } = require('../controllers');
-const { validate, createUser, getUsers, getUser, updateUser } = userController;
+const {
+  validate,
+  createUser,
+  getUsers,
+  getUser,
+  updateUser,
+  participateEvent,
+} = userController;
 
 router.post('/', validate('createUser'), createUser);
 router.get('/', getUsers);
-router.get('/:id', getUser)
-router.put('/:id', validate('updateUser'), updateUser)
+router.get('/:id', getUser);
+router.put('/:id', validate('updateUser'), updateUser);
+router.post(
+  '/:id/events/:eventId',
+  validate('participateEvent'),
+  participateEvent
+);
 
 module.exports = router;
