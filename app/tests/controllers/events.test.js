8e1160d628bd29a6aca44c012ab9359345e33a72
shahrour <ahmadshahrour951@gmail.com>
2021-07-03 18:25:57 -0700

    createEvent unittests

--- app/tests/controllers/events.test.js
+++ app/tests/controllers/events.test.js
@@ -4,18 +4,18 @@ const app = require('../../../server');
 
 const { expect } = chai;
 
-const event1 = {
-  id: 1,
-  name: 'Running',
-  startTime: '2021-08-02T03:00:00.000Z',
-  endTime: '2021-08-02T04:00:00.000Z',
-  CreatorUserId: 1,
-  ResidenceId: 1,
-  createdAt: '1995-11-18T00:00:00.000Z',
-  updatedAt: '1995-11-18T00:00:00.000Z',
-};
+describe('Fetch Events', async () => {
+  const event1 = {
+    id: 1,
+    name: 'Running',
+    startTime: '2021-08-02T03:00:00.000Z',
+    endTime: '2021-08-02T04:00:00.000Z',
+    CreatorUserId: 1,
+    ResidenceId: 1,
+    createdAt: '1995-11-18T00:00:00.000Z',
+    updatedAt: '1995-11-18T00:00:00.000Z',
+  };
 
-describe('Fetch events test', async () => {
   it('should fetch all events', async () => {
     const { body, status } = await request(app).get('/events');
     const { data } = body;
@@ -32,5 +32,115 @@ describe('Fetch events test', async () => {
 
     expect(status).to.equal(200);
     expect(event).to.be.an('object').to.eql(event1);
+  });
+});
+
+describe('Create Events', async () => {
+  const successEvent = {
+    id: 2,
+    name: 'Pancakes and Eggs',
+    startTime: new Date('2021-08-01 11:00:00'),
+    endTime: new Date('2021-08-01 13:00:00'),
+    CreatorUserId: 1,
+    ResidenceId: 1,
+  };
+
+  const failEvent1 = {
+    id: 3,
+    name: 'Pancakes and Eggs',
+    startTime: new Date('2021-08-01 13:00:00'),
+    endTime: new Date('2021-08-01 11:00:00'),
+    CreatorUserId: 1,
+    ResidenceId: 1,
+    createdAt: new Date('1995-11-18'),
+    updatedAt: new Date('1995-11-18'),
+  };
+
+  const failEvent2 = {
+    name: 'Pancakes and Eggs',
+    startTime: new Date('2021-08-01 11:00:00'),
+    endTime: new Date('2021-08-01 1:00:00'),
+    ResidenceId: 1,
+    createdAt: new Date('1995-11-18'),
+    updatedAt: new Date('1995-11-18'),
+  };
+
+  const failEvent3 = {
+    name: 'Pancakes and Eggs',
+    startTime: new Date('2021-08-01 1:00:00'),
+    endTime: new Date('2021-08-01 11:00:00'),
+    CreatorUserId: 1,
+    createdAt: new Date('1995-11-18'),
+    updatedAt: new Date('1995-11-18'),
+  };
+
+  it('should successfully create an event', async () => {
+    const { body, status } = await request(app)
+      .post('/events')
+      .send(successEvent);
+    const { message, data } = body;
+    const { id } = data;
+
+    expect(status).to.equal(201);
+    expect(message).to.be.a('string').to.equal('Event successfully created.');
+    expect(id).to.be.a('number').to.equal(2);
+  });
+
+  it('should fail to create an event - startTime after endTime', async () => {
+    const { body, status } = await request(app)
+      .post('/events')
+      .send(failEvent1);
+    const { data } = body;
+    const { errors } = data;
+
+    expect(status).to.equal(500);
+    expect(errors)
+      .to.be.an('array')
+      .to.include.deep.members([
+        {
+          value: '2021-08-01T18:00:00.000Z',
+          msg: 'startTime must be before endTime.',
+          param: 'endTime',
+          location: 'body',
+        },
+      ]);
+  });
+
+  it('should fail to create an event - No creator', async () => {
+    const { body, status } = await request(app)
+      .post('/events')
+      .send(failEvent2);
+    const { data } = body;
+    const { errors } = data;
+
+    expect(status).to.equal(500);
+    expect(errors)
+      .to.be.an('array')
+      .to.include.deep.members([
+        {
+          msg: 'CreatorUserId must be provided.',
+          param: 'CreatorUserId',
+          location: 'body',
+        },
+      ]);
+  });
+
+  it('should fail to create an event - No residence', async () => {
+    const { body, status } = await request(app)
+      .post('/events')
+      .send(failEvent3);
+    const { data } = body;
+    const { errors } = data;
+
+    expect(status).to.equal(500);
+    expect(errors)
+      .to.be.an('array')
+      .to.include.deep.members([
+        {
+          msg: 'ResidenceId must be provided.',
+          param: 'ResidenceId',
+          location: 'body',
+        },
+      ]);
   });
 });
