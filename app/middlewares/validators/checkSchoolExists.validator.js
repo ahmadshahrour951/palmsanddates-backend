09d28a1050ebb6e812a05d8e656c6b94f1900d3d
shahrour <ahmadshahrour951@gmail.com>
2021-07-07 20:03:50 -0700

    getEvents for schools route

--- app/middlewares/validators/checkSchoolExists.validator.js
+++ app/middlewares/validators/checkSchoolExists.validator.js
@@ -2,7 +2,16 @@ const db = require('../../db/models');
 
 const checkSchoolExists = async (value, { req, location, path }) => {
   const school = await db.School.findByPk(value);
-  return school !== undefined && school !== null;
+  const isValid = school !== undefined && school !== null;
+
+  if (!isValid) {
+    const error = new Error('School not found');
+    error.statusCode = 404;
+    throw error;
+  }
+
+  req.School = school;
+  return isValid;
 };
 
 module.exports = checkSchoolExists;
--- app/routes/school.route.js
+++ app/routes/school.route.js
@@ -2,12 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 const { schoolController } = require('../controllers');
-const { validate, createSchool, getSchools, getSchool, updateSchool } =
-  schoolController;
+const {
+  validate,
+  createSchool,
+  getSchools,
+  getSchool,
+  updateSchool,
+  getEvents,
+} = schoolController;
 
 router.post('/', validate('createSchool'), createSchool);
 router.get('/', getSchools);
 router.get('/:id', getSchool);
 router.put('/:id', validate('updateSchool'), updateSchool);
+
+router.get('/:id/events', validate('getEvents'), getEvents);
 
 module.exports = router;
